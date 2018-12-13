8fd980be3fc6efb6f88d73314e9efe68ac859c56
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2018-12-13 10:17:45 +0100

    Day8, part 2

--- src/nl/vloris/adventofcode/advent2018/day8.kt
+++ src/nl/vloris/adventofcode/advent2018/day8.kt
@@ -7,14 +7,14 @@ object Day8 {
         .filter { it.isNotEmpty() }
         .map { it.toInt() }
 
-    private fun parseNode(iterator: Iterator<Int>): Int {
+    private fun parseNode1(iterator: Iterator<Int>): Int {
         val numChilds = iterator.next()
         val numMetadata = iterator.next()
 
         var runningTotal = 0
 
         for (i in 0 until numChilds) {
-            runningTotal += parseNode(iterator)
+            runningTotal += parseNode1(iterator)
         }
 
         for (i in 0 until numMetadata) {
@@ -28,10 +28,46 @@ object Day8 {
     fun answerPart1(): Int {
         val iterator = input.iterator()
 
-        return parseNode(iterator)
+        return parseNode1(iterator)
+    }
+
+    private fun parseNode2(iterator: Iterator<Int>): Int {
+        val numChilds = iterator.next()
+        val numMetadata = iterator.next()
+
+        val childNodes = mutableListOf<Int>()
+        for (i in 0 until numChilds) {
+            childNodes.add(parseNode2(iterator))
+        }
+
+        var runningTotal = 0
+        // 1. no childs? add metadata together
+        if (numChilds == 0) {
+            for (i in 0 until numMetadata) {
+                val metadata = iterator.next()
+                runningTotal += metadata
+            }
+        }
+        // 2. childs? metadata are indices to childnodes
+        else {
+            for (i in 0 until numMetadata) {
+                val metadata = iterator.next()
+                if (metadata > childNodes.size) continue
+                runningTotal += childNodes[metadata-1]
+            }
+        }
+
+        return runningTotal
+    }
+
+    fun answerPart2(): Int {
+        val iterator = input.iterator()
+
+        return parseNode2(iterator)
     }
 }
 
 fun main(args: Array<String>) {
     println("Day 8, part 1: " + Day8.answerPart1())
+    println("Day 8, part 2: " + Day8.answerPart2())
 }
