9f8d84b8723cedb481d553ab6536f4ca85312cc5
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2018-12-17 15:44:11 +0100

    Day14, part2

--- src/nl/vloris/adventofcode/advent2018/day14.kt
+++ src/nl/vloris/adventofcode/advent2018/day14.kt
@@ -25,6 +25,17 @@ object Day14 {
                 .joinToString("")
         }
 
+        fun countRecipesBefore(pattern: String): Int {
+            val matchList = pattern.toCharArray().map { Recipe(it.toInt() - '0'.toInt()) }
+
+            var index: Int?
+            do {
+                nextRound()
+                index = scoreBoardEndsWith(matchList)
+            } while (index == null)
+            return index
+        }
+
         private fun nextRound() {
             val currentRecipes = elves.map { elf -> recipes[elf.currentIndex] }
             val newRecipes = createNewRecipes(currentRecipes)
@@ -44,6 +55,20 @@ object Day14 {
                 listOf(Recipe(sum / 10), Recipe(sum % 10))
             } else {
                 listOf(Recipe(sum))
+            }
+        }
+
+        private fun scoreBoardEndsWith(matchList: List<Recipe>): Int? {
+            if (recipes.size <= matchList.size) return null
+
+            // check also for match one earlier in case two recipes where added
+            val list1 = recipes.subList(recipes.size - matchList.size, recipes.size)
+            val list2 = recipes.subList(recipes.size - matchList.size - 1, recipes.size - 1)
+
+            return when (matchList) {
+                list1 -> recipes.size - matchList.size
+                list2 -> recipes.size - matchList.size - 1
+                else -> null
             }
         }
 
@@ -67,8 +92,14 @@ object Day14 {
         val scoreBoard = ScoreBoard(listOf(Recipe(3), Recipe(7)))
         return scoreBoard.findScoreAfter(190221)
     }
+
+    fun answerPart2(): Int {
+        val scoreBoard = ScoreBoard(listOf(Recipe(3), Recipe(7)))
+        return scoreBoard.countRecipesBefore("190221")
+    }
 }
 
 fun main(args: Array<String>) {
     println("Day14, part1: " + Day14.answerPart1())
+    println("Day14, part2: " + Day14.answerPart2())
 }
